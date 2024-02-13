ea32ce4076ebe88d144d7515f8e7b7d453a04714
Shivam Chaubey <shivam@Shivams-MacBook-Air-2.local>
2024-02-14 00:15:02 +0530

    frontend config

--- Back_End/user/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def say_hello(request):
-    return render(request, 'hello.html', {'name': 'Mosh'})
-
-def index(request):
-    return HttpResponse('<h1>Welcome to Social book</h1>')
